fe9d4fb4f6e8680ffc2ed682eec99eb399ba38c5
rhapsodic-numrod <nandjebo.dev@gmail.com>
2024-03-18 11:58:23 +0200

    Updated MainActivity

--- app/src/main/java/com/numrod/moneyforker/MainActivity.kt
+++ app/src/main/java/com/numrod/moneyforker/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MyApp()
                 }
             }
         }
@@ -30,17 +30,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyApp(modifier: Modifier = Modifier) {
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MyAppPreview() {
     MoneyForkerTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
